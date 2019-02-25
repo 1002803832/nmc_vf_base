0492ab572a4d837c19070daa360102d5e20d51cf
liucouhua <liucouhua@163.com>
2019-02-25 14:54:04 +0800

    update setup.py
    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,46 @@
+# _*_ coding: utf-8 _*_
 
-from distutils.core import setup
+from os import path
+from setuptools import find_packages, setup
+from codecs import open
 
-setup(name='nmc_vf_base',
-    version='1.0',
-    description='nmc_verification_base',
-    author='Liu couhua',
+name = "nmc_vf_base"
+author = __import__(name).__author__
+version = __import__(name).__version__
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name=name,
+
+    version=version,
+
+    description=("A collections of basic functions "
+                 "for meteorological verification."),
+    long_description=long_description,
+
+    # author
+    author=author,
     author_email='liucouhua@163.com',
-    url='https://github.com/nmcdev/nmc_vf_base.git',
-    py_modules=['binary'],)
+
+    # LICENSE
+    license='GPL3',
+
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python :: 3',
+    ],
+
+    packages=find_packages(exclude=['docs', 'tests', 'build', 'dist']),
+    include_package_data=True,
+    exclude_package_data={'': ['.gitignore']},
+
+    install_requires=['numpy>=1.12.1',
+                      'scipy>=0.19.0',
+                      'pandas>=0.20.0',]
+)
