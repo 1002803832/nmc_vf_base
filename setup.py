806408898082bc2094864db58fcf2c0fab8478df
liucouhua <liucouhua@163.com>
2019-02-25 11:41:40 +0800

    添加了MANIFAST.in setup.py 和一个二分类预报检验模块
    添加了添加了MANIFAST.in setup.py 和一个二分类预报检验模块，该模块主要用于计算ts和ets评分

---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(name='nmc_vf_base',
+    version='1.0',
+    description='nmc_verification_base',
+    author='Liu couhua',
+    author_email='liucouhua@163.com',
+    url='https://github.com/nmcdev/nmc_vf_base.git',
+    py_modules=['binary'],)
